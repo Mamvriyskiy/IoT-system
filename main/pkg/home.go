51af06655b2df78404b9a2e4245c13578d220018
Mamvriyskiy <68384944+Mamvriyskiy@users.noreply.github.com>
2024-10-08 16:22:36 +0300

    update intro and endToEnd tests

--- main/pkg/home.go
+++
@@ -1,8 +0,0 @@
-package pkg
-
-type Home struct {
-	Name      string  `db:"name"    json:"name"`
-	Latitude  float64 `db:"latitude" json:"latitude"`
-	Longitude float64 `db:"longitude" json:"longitude"`
-	ID        string     `db:"homeid"  json:"homeid"`
-}
--- main/research/migrations/indx/20240823115142_index.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-CREATE INDEX index_login ON client (login);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-
--- +goose StatementEnd
